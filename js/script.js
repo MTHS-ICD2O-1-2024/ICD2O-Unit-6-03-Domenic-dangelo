1c1e7dbe9efcb5c3afa5d5883acecc24526851fc
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-22 16:11:39 +0000

    not done JSON and API weather

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function gets the Cat fact.
+ * The "async" is there because it will take time for the internet to return the value
+ */
+async function calculate() {
+  try {
+    const resultJSON = await fetch("https://samples.openweathermap.org/data/2.5/weather")
+    const jsonData = await resultJSON.json()
+    console.log(jsonData)
+    const tempurature = jsonData.data[temp]
+
+    // output
+    document.getElementById("cat-fact").innerHTML = "<p>" + tempurature + "</p>"
+  } 
+  catch (error) {
+    console.error(error)
+  }
+}
