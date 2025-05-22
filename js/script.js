e930e7cc08b603cd3c1a1ee70f5c544e049f2c28
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-22 23:25:33 +0000

    weather

--- js/script.js
+++ js/script.js
@@ -10,17 +10,22 @@
  * This function gets the Cat fact.
  * The "async" is there because it will take time for the internet to return the value
  */
-async function calculate() {
+async function weather() {
   try {
-    const resultJSON = await fetch("https://samples.openweathermap.org/data/2.5/weather")
+    const resultJSON = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
     const jsonData = await resultJSON.json()
     console.log(jsonData)
-    const tempurature = jsonData.data[temp]
+    const weatherDescription = jsonData.weather[0].description
+    const weatherIconCode = jsonData.weather[0].icon
+    const weatherIconURL = "https://openweathermap.org/img/wn/" + weatherIconCode + "@2x.png"
+    const currentWeatherKelvin = jsonData.main.temp
+    const currentWeatherCelcius = currentWeatherKelvin - 273.15
 
     // output
-    document.getElementById("cat-fact").innerHTML = "<p>" + tempurature + "</p>"
-  } 
+    document.getElementById("result").innerHTML = "<p> The current temperature is: " + currentWeatherCelcius.toFixed(0) + "℃. </p> <br /> <p> The current weather is " + weatherDescription + ". <br /> <img src =" + weatherIconURL + " alt='Weather Icon'>"
+  }
+
   catch (error) {
-    console.error(error)
+    document.getElementById("result").innerHTML = "Sorry, there is an error at this moment."
   }
 }
